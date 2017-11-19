cca50520d083150a27d9edfc0ae157bd2e4582d7
Michael Cordero <michaelpetercordero@gmail.com>
2017-11-19 15:44:07 -0600

    End Lesson # 26

--- IntermediateTraining/Controllers/Companies/CompaniesViewController.swift
+++ IntermediateTraining/Controllers/Companies/CompaniesViewController.swift
@@ -21,7 +21,7 @@ class CompaniesViewController: UITableViewController {
         self.companies = CoreDataManager.shared.fetchCompanies()
         // Create Buttons
         navigationItem.leftBarButtonItems = [UIBarButtonItem.init(title: "Reset", style: .plain, target: self, action: #selector(handleReset)),
-                                             UIBarButtonItem.init(title: "Do Work", style: .plain, target: self, action: #selector(doWork))]
+                                             UIBarButtonItem.init(title: "Do Updates", style: .plain, target: self, action: #selector(doUpdates))]
         // Create Navigation Controller UI
         view.backgroundColor = UIColor.white
         navigationItem.title = "Companies"
@@ -60,17 +60,45 @@ class CompaniesViewController: UITableViewController {
     @objc private func doWork() {
         print("Trying to do work...")
         CoreDataManager.shared.persistentContainer.performBackgroundTask({ (backgroundContext) in
-            (0...10000).forEach { (value) in
+            (0...5).forEach { (value) in
                 print(value)
                 let company = Company(context: backgroundContext)
                 company.name = String(value)
             }
             do {
                 try backgroundContext.save()
+                DispatchQueue.main.async {
+                    self.companies = CoreDataManager.shared.fetchCompanies()
+                    self.tableView.reloadData()
+                }
             } catch let err {
                 print("Failed to save: ", err)
             }
         })
+    }
+    
+    @objc private func doUpdates() {
+        CoreDataManager.shared.persistentContainer.performBackgroundTask { (backgroundContext) in
+            let request: NSFetchRequest<Company> = Company.fetchRequest()
+            do {
+                 let companies = try backgroundContext.fetch(request)
+                companies.forEach({ print( $0.name ?? ""); $0.name = "C: \($0.name ?? "")"})
+                do {
+                    try backgroundContext.save()
+                    DispatchQueue.main.async {
+                        // reset will forget all the objects you've fetched before!
+                        CoreDataManager.shared.persistentContainer.viewContext.reset()
+                        //is there a way to merge the changes that you just made onto the view context?
+                        self.tableView.reloadData()
+                    }
+                } catch let saveError {
+                    print("Failed to save on background: ", saveError)
+                }
+            } catch let fetchError {
+                print("Failed to fetch companies: ", fetchError)
+            }
+            
+        }
     }
   
 }
